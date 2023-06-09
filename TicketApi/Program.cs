0b2e62d3e32bb4e7e32ff54975d4d8839c318c6c
caarlo0s <rricardez@live.com.ar>
2023-06-08 19:09:28 -0600

    Agregar configuraciones para conexxion a bd e injeccion de dependencias configuracion de swagger y CORSS, Autorización de JWT Token

--- TicketApi/Program.cs
+++ TicketApi/Program.cs
@@ -1,11 +1,56 @@
+using System.Text;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using SterenTicketsBE.ConfigurationServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
+
+
 // Add services to the container.
+builder.Services.AddCors(options =>
+                        {
+                            options.AddDefaultPolicy(builder =>
+                            {
+                                builder
+                                // .WithOrigins("http://127.0.0.1:5503/")
+                                .WithOrigins("*")
+                                .AllowAnyMethod()
+                                .AllowAnyHeader()
+                                .SetIsOriginAllowed(origin => true);
+                            });
+                        });
+
+builder.Services.AddControllers(
+opt =>
+{
+var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+opt.Filters.Add(new AuthorizeFilter(policy));
+});
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers().AddJsonOptions(x =>
+                x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+var key = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(builder.Configuration.GetSection("JwtConfig").GetValue<string>("Secret")));
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>{
+    opt.TokenValidationParameters = new TokenValidationParameters{
+        ValidateIssuerSigningKey    = true,
+        IssuerSigningKey            = key,
+        ValidateAudience            = false,
+        ValidateIssuer              = false
+    };
+});
+builder.Services.AddInjectionDependencies();
 
 var app = builder.Build();
 
@@ -13,13 +58,24 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>{
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApi v1");
+                    c.RoutePrefix = string.Empty; 
+                });
 }
-
 app.UseHttpsRedirection();
+
+app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllers();
+app.UseCors();
+//se agrega para authentication
+app.UseAuthentication();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
 
 app.Run();
